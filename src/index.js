370c692b25fb9e4f8a9c195b85528a8eae9a5515
Mohit Kumar <kmohit9111@gmail.com>
2020-06-06 01:22:22 +0530

    add sort and filter

--- src/index.js
+++ src/index.js
@@ -13,18 +13,3 @@ app.use(taskRouter)
 app.listen(port,()=>{
     console.log("Server is up on port ",port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async()=>{
-    // const task = await Task.findById('5ed7ba0169c5960e0c8d60f6')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    const user = await User.findById('5ed7b93e294628432c6c4239')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
-
-main()
